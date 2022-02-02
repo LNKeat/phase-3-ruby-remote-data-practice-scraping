025acc2ff9fe88f7d50277ea1a3ae544d538a9fb
Laura <lnkeat@gmail.com>
2022-02-01 17:03:09 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+ header = doc.css("  #post-5 > div > section.hero-image-block.image-block.image-static > div > div > div > div > h1").text.strip
+
+courses = doc.css(".heading-20-semibold.color-black.card-title-tl"
+
+binding.pry
+0
